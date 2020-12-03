35cf30f441f72f2978bd366dcc9de668caed6e9a
Gabriel Miranda <gabrielpmjp@gmail.com>
2020-12-03 18:37:37 -0300

    Mudanças: 	- Sons para os golpes tocando ao realizar o movimento; 	- 4 arquivos de áudio como efeitos sonoros de luta.

--- Assets/Scripts/FightManager.cs
+++ Assets/Scripts/FightManager.cs
@@ -5,24 +5,35 @@ using UnityEngine.UI;
 
 public enum Move {UP, DOWN, LEFT, RIGHT}
 public class Fighter {
+    public int games = 0;
     public int hP = 0;
 }
 public class FightManager : MonoBehaviour
 {
     //Esta classe inicializa duas listas, uma para os movimentos recorrentes da luta, e outra que abriga os temporários do jogador atual
     public int hpInicial = 3;
+    public int gamesToWin = 2;
     public List<Move> fightMoves = new List<Move>();
     public List<Move> tempMoves = new List<Move>();
     [SerializeField] private ShiftManagementScript shiftManagementScript;
     [SerializeField] private Text playerHP;
+    [SerializeField] private Text playerRounds;
+    [SerializeField] private Text enemyRounds;
     [SerializeField] private Text enemyHP;
     [SerializeField] private Text lastMove;
+    //Os áudios desejados devem ser atribuídos no editor
+    [SerializeField] private AudioClip[] punchSounds = new AudioClip[4];
+    private AudioSource audioSourceComponent;
     public Fighter playerFighter = new Fighter();
     public Fighter enemyFighter = new Fighter();
 
 
     //No início, os HP dos lutadores são determinados
     private void Start() {        
+        audioSourceComponent = GetComponent<AudioSource>();
+        if(audioSourceComponent == null) {
+            Debug.LogWarning("AudioSource não encontrado");
+        }
         playerFighter.hP = hpInicial;
         enemyFighter.hP = hpInicial;
         UpdateUI();
@@ -39,11 +50,13 @@ public class FightManager : MonoBehaviour
         //e é checado se a sequência atual é correta.
         if(tempMoves.Count < fightMoves.Count) {
             tempMoves.Add(newMove);
-             lastMove.text = newMove.ToString();
+            //O áudio do golpe é tocado uma vez
+            audioSourceComponent.PlayOneShot(punchSounds[(int)newMove]);
+            lastMove.text = newMove.ToString();
 
             //Caso a sequência não esteja correta, o personagem leva dano
             if(!RightSequence()) {
-                shiftManagementScript.UIText.text = "Dano recebido!";
+                lastMove.text = "Dano recebido!";
                 TakeDamage();
             }
             return;
@@ -51,6 +64,8 @@ public class FightManager : MonoBehaviour
         
         //Se a sequência estiver correta, o golpe é adicionado à lista principal e a lista temporária é zerada
         fightMoves.Add(newMove);
+        //O áudio do golpe é tocado uma vez
+        audioSourceComponent.PlayOneShot(punchSounds[(int)newMove]);
         lastMove.text = newMove.ToString();
         tempMoves = new List<Move>();
                 
@@ -97,13 +112,22 @@ public class FightManager : MonoBehaviour
     }
 
     //No fim do jogo, o texto da UI é atualizado com a mensagem de vitória ou derrota
-    //Além disso, as vidas são reiniadas
+    //Além disso, o game do lutador respectivo é contabilizado e as vidas são reiniciadas
     public void EndGame() {
         if(playerFighter.hP > 0) {
-            shiftManagementScript.UIText.text = "Você ganhou! :DDDD";
+            playerFighter.games++;
+            lastMove.text = "Round ganho";
         } else {
-            shiftManagementScript.UIText.text = "Você perdeu! :'(";
+            enemyFighter.games++;
+            lastMove.text = "Round perdido";
         }
+
+        if(playerFighter.games == gamesToWin) {
+            lastMove.text = "Você ganhou a luta! :DDDD";
+        } else if(enemyFighter.games == gamesToWin) {
+            lastMove.text = "Você perdeu a luta! :(";
+        }
+
         playerFighter.hP = hpInicial;
         enemyFighter.hP = hpInicial;
         UpdateUI();
@@ -112,5 +136,7 @@ public class FightManager : MonoBehaviour
     public void UpdateUI() {
         playerHP.text = "HP: " + playerFighter.hP;
         enemyHP.text = "HP: " + enemyFighter.hP;
+        playerRounds.text = "Rounds: " + playerFighter.games;
+        enemyRounds.text = "Rounds: " + enemyFighter.games;
     }
 }
--- Assets/Scripts/FighterAI.cs
+++ Assets/Scripts/FighterAI.cs
@@ -9,7 +9,8 @@ public class FighterAI : MonoBehaviour
     //A primeira é a chance de acertar um golpe. O valor escolhido no editor é o valor inicial.
     //Este valor descresce pela quantidade de "decreaseChanceByMove" a cada movimento executado.
     //(Discutir outras possíveis abordagens para a IA)
-    [Range(0,100)] public float hitChance = 95f;
+    [Range(0,100)] public float inicialHitChance = 95f;
+    [SerializeField]private float hitChance = 0;
     public float decreaseChanceByMove = 0.5f;
     private bool enableCoroutine = true;
     [SerializeField] private FightManager fightManagerScript;
@@ -17,6 +18,7 @@ public class FighterAI : MonoBehaviour
     
     void Awake()
     {
+        hitChance = inicialHitChance;
          //A classe encontra o objeto do tipo FightManager e atribui à variável devida.
         fightManagerScript = GameObject.FindObjectOfType<FightManager>();
         if(fightManagerScript == null) {
@@ -75,6 +77,10 @@ public class FighterAI : MonoBehaviour
     //O golpe é executado e a chance de acertar o próximo diminui de acordo com "decreaseChanceByMove"
         fightManagerScript.PerformMove(toPerform);
         hitChance -= decreaseChanceByMove;
+
+        if(fightManagerScript.fightMoves.Count == 0) {
+            hitChance = inicialHitChance;
+        }
     }
 
     //Esta corotina executa um cálculo a cada segundo enquanto for o turno do inimigo, depois habilita a chamada dela mesma 
